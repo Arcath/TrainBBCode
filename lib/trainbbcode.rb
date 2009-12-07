879745db92946bdd9f8e9f0d13ad17540f791540
Adam Laycock <arcath@whitefall.(none)>
2009-12-07 20:31:49 +0000

    Made Configurable

--- lib/trainbbcode.rb
+++ lib/trainbbcode.rb
@@ -1,17 +1,43 @@
 class TBBC
+	def initialize
+		self.conf(:configed_by => "system")
+	end
+	def conf(config)
+		config[:configed_by] ||= "user"
+		config[:image_alt] ||= "Posted Image" 
+		config[:url_target] ||= "_BLANK"
+		
+		@config=config
+	end
 	def parse(s)
 		#First off remove the < and > which will disable all HTML
 		s=s.gsub("<","&lt;").gsub(">","&gt;")
 		#Convert new lines to <br />'s
 		s=s.gsub(/\n/,'<br />')
-		#Tags
-		s=s.gsub(/\[b\](.*?)\[\/b\]/,'<strong>\1</strong>')
-		s=s.gsub(/\[i\](.*?)\[\/b\]/,'<i>\1</i>')
-		s=s.gsub(/\[u\](.*?)\[\/u\]/,'<u>\1</u>')
-		s=s.gsub(/\[url\](.*?)\[\/url\]/,'<a href="\1" target="_BLANK">\1</a>')
-		s=s.gsub(/\[url=(.*?)\](.*?)\[\/url\]/,'<a href="\1" target="_BLANK">\2</a>')
+		#Loading Custom Tags
+		if @config[:custom_tags] then
+			@config[:custom_tags].each do |tag|
+				s=s.gsub(tag[0],tag[1]) unless tag[2] == false
+			end
+		end
+		#Loading Default Tags and applying them
+		tags=load_default_tags
+		tags.each do |tag|
+			s=s.gsub(tag[0],tag[1]) unless tag[2] == false
+		end	
 		
 		return s
+	end
+	def load_default_tags
+		tags=[
+			[/\[b\](.*?)\[\/b\]/,'<strong>\1</strong>',@config[:strong_enabled]],
+			[/\[i\](.*?)\[\/i\]/,'<i>\1</i>',@config[:italic_enabled]],
+			[/\[u\](.*?)\[\/u\]/,'<u>\1</u>',@config[:underline_enabled]],
+			[/\[url\](.*?)\[\/url\]/,'<a href="\1" target="' + @config[:url_target] +'">\1</a>',@config[:url_enabled]],
+			[/\[url=(.*?)\](.*?)\[\/url\]/,'<a href="\1" target="' + @config[:url_target] + '">\2</a>',@config[:url_enabled]],
+			[/\[img\](.*?)\[\/img\]/,'<img src="\1" alt="'+ @config[:image_alt] + '" />',@config[:image_enabled]],
+			[/\[img alt=(.*?)\](.*?)\[\/img\]/,'<img src="\2" alt="\1" />',@config[:image_enabled]]
+		]
 	end
 end
 
@@ -21,3 +47,8 @@ class String
 		bbc.parse(self)
 	end
 end
+
+s="[img alt=testing]test[/img]"
+t=TBBC.new
+t.conf(:custom_tags => [[/\[s\](.*?)\[\/s\]/,'<strikethrough>\1</strikethrough>',true]], :image_enabled => false)
+puts t.parse(s)
